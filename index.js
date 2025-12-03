8add64e761ea337624aebfde1974b04fecda9518
Arbaz khan <79788148+Sahil-6757@users.noreply.github.com>
2025-12-03 17:00:35 +0530

    Return user data on successful login

--- index.js
+++ index.js
@@ -103,11 +103,11 @@ app.post("/contact", async (req, res) => {
 app.post("/login", async (req, res) => {
   let result = await User.find(req.body);
   let admin = await Admin.find(req.body);
-  console.log(admin);
   if (result.length > 0) {
-    console.log(result);
+    res.json({user:result})
     res.json({ message: "login Success" });
   } else if (admin.length > 0) {
+    res.json({user:admin})
     res.json({ message: "Admin login Success" });
   } else {
     res.json({ message: "login Failed" });
